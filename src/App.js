5519f7ac941b546a72dd95f3a89d417e8eb3c09a
Hung Tran <tranngochung0510@gmail.com>
2019-11-26 21:33:22 +0700

    fix UI

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import EventNameInput from './EventNameInput.jsx';
 import './App.css';
 
 function App() {
-	const { width, height } = useWindowSize();
-	const [show, setShow] = useState(false);
-	const [customer, setCustomer] = useState({});
-	const [events, setEvents] = useState([]);
-	const [customers, setCustomers] = useState([]);
-	const [disable, setDisable] = useState(true);
-	/* useEffect(() => {
+    const { width, height } = useWindowSize();
+    const [show, setShow] = useState(false);
+    const [customer, setCustomer] = useState({});
+    const [events, setEvents] = useState([]);
+    const [customers, setCustomers] = useState([]);
+    const [disable, setDisable] = useState(true);
+    /* useEffect(() => {
 		const $ = s => document.querySelector(s);
 		var sepa = new Flip({
 			node: $(".separate"),
@@ -34,40 +34,40 @@ function App() {
 		};
 	}, []); */
 
-	useEffect(() => {
-		axios
-			.get(
-				'http://localhost:3001/graphql?query={getEvents{eventName,id}}'
-			)
-			.then(res => {
-				// console.log(res.data.data.getEvents);
-				setEvents(res.data.data.getEvents);
-			})
-			.catch(error => console.log(error));
-	}, []);
+    useEffect(() => {
+        axios
+            .get(
+                'http://localhost:3001/graphql?query={getEvents{eventName,id}}'
+            )
+            .then(res => {
+                // console.log(res.data.data.getEvents);
+                setEvents(res.data.data.getEvents);
+            })
+            .catch(error => console.log(error));
+    }, []);
 
-	function findWinner() {
-		if (customers === []) return;
-		let tmp = [...customers];
-		tmp.splice(customers.indexOf(customer), 1);
-		let index = Math.floor(Math.random() * tmp.length);
-		let winner = tmp[index];
-		console.log('winner', winner);
-		window.localStorage.setItem('result', parseInt(winner.code));
-		setCustomers(tmp);
-		setCustomer(winner);
-	}
+    function findWinner() {
+        if (customers === []) return;
+        let tmp = [...customers];
+        tmp.splice(customers.indexOf(customer), 1);
+        let index = Math.floor(Math.random() * tmp.length);
+        let winner = tmp[index];
+        console.log('winner', winner);
+        window.localStorage.setItem('result', parseInt(winner.code));
+        setCustomers(tmp);
+        setCustomer(winner);
+    }
 
-	function spin() {
-		if (customers === []) alert('Het khach hang');
-		setTimeout(() => {
-			setShow(true);
-		}, 7500);
-	}
-	function getCustomers(data) {
-		axios
-			.post(`http://localhost:3001/graphql`, {
-				query: `
+    function spin() {
+        if (customers === []) alert('Het khach hang');
+        setTimeout(() => {
+            setShow(true);
+        }, 7500);
+    }
+    function getCustomers(data) {
+        axios
+            .post(`http://localhost:3001/graphql`, {
+                query: `
 					query GetCustomerByEvent($eventName: String!) {
 						getCustomerByEvent(eventName: $eventName) {
 							name,
@@ -75,91 +75,100 @@ function App() {
 							code
 					}
 					}`,
-				variables: {
-					eventName: data
-				}
-			})
-			.then(res => {
-				let _customers = res.data.data.getCustomerByEvent;
-				let index = Math.floor(Math.random() * _customers.length);
-				let winner = _customers[index];
-				console.log('winner', winner);
-				window.localStorage.setItem('result', parseInt(winner.code));
-				setCustomers(_customers);
-				setDisable(false);
-				setCustomer(winner);
-			})
-			.catch(error => {
-				console.log(error);
-				alert(`Danh sach khach hang trong`);
-			});
-	}
-	return (
-		<>
-			<div className="header">
-				<img src={headerImg} alt="" />
-				<p>CHÚC MỪNG KHÁCH HÀNG MAY MẮN</p>
-			</div>
-			<div className="box">
-				<div className="separate"></div>
-				<button
-					className="btn-start"
-					onClick={() => {
-						spin();
-					}}
-					disabled={disable}
-				>
-					Quay
-				</button>
-				<EventNameInput
-					getCustomers={data => {
-						getCustomers(data);
-					}}
-					events={events}
-				></EventNameInput>
-			</div>
-			{show ? (
-				<>
-					<div className="confetti">
-						<Confetti width={width} height={height} />
-					</div>
-					<div className="reward">
-						<img src={reward} alt=""></img>
-						<div className="info">
-							{/* <div>
+                variables: {
+                    eventName: data
+                }
+            })
+            .then(res => {
+                let _customers = res.data.data.getCustomerByEvent;
+                let index = Math.floor(Math.random() * _customers.length);
+                let winner = _customers[index];
+                console.log('winner', winner);
+                window.localStorage.setItem('result', parseInt(winner.code));
+                setCustomers(_customers);
+                setDisable(false);
+                setCustomer(winner);
+            })
+            .catch(error => {
+                console.log(error);
+                alert(`Danh sach khach hang trong`);
+            });
+    }
+    return (
+        <>
+            <div className="header">
+                <img src={headerImg} alt="" />
+                <p>CHÚC MỪNG KHÁCH HÀNG MAY MẮN</p>
+            </div>
+            <div className="box">
+                <div className="separate"></div>
+                <div>
+                    <button
+                        className="btn-start"
+                        onClick={() => {
+                            spin();
+                        }}
+                        disabled={disable}
+                    >
+                        Quay
+                    </button>
+                    <select name="prize" className="btn-start select-prize">
+                        <option value="1" defaultValue>
+                            Giải nhất
+                        </option>
+                        <option value="2">Giải nhì</option>
+                        <option value="3">Giải ba</option>
+                    </select>
+                </div>
+                <EventNameInput
+                    getCustomers={data => {
+                        getCustomers(data);
+                    }}
+                    events={events}
+                ></EventNameInput>
+            </div>
+            {show ? (
+                <>
+                    <div className="confetti">
+                        <Confetti width={width} height={height} />
+                    </div>
+                    <div className="reward">
+                        <img src={reward} alt=""></img>
+                        <div className="info">
+                            {/* <div>
 								<span>Kết quả :</span>
 								<b>{window.localStorage.getItem("result")}</b>
 							</div> */}
-							<div>
-								<span>Họ và tên :</span>
-								<b>{customer.name}</b>
-							</div>
-							<div>
-								<span>Số điện thoại :</span>
-								<b>{customer.phone}</b>
-							</div>
-							<div>
-								<span>Mã dự thưởng :</span>
-								<b>{customer.code}</b>
-							</div>
-						</div>
-						<button
-							className="btn-close"
-							onClick={() => {
-								setShow(false);
-								findWinner();
-							}}
-						>
-							Đóng
-						</button>
-					</div>
-					<div className="mask"></div>
-				</>
-			) : (
-				<div></div>
-			)}
-		</>
-	);
+                            <div>
+                                <span>Họ và tên :</span>
+                                <b>{customer.name}</b>
+                            </div>
+                            <div>
+                                <span>Số điện thoại :</span>
+                                <b>{customer.phone}</b>
+                            </div>
+                            <div>
+                                <span>Mã dự thưởng :</span>
+                                <b>{customer.code}</b>
+                            </div>
+                        </div>
+                        <button
+                            className="btn-close"
+                            onClick={() => {
+                                setShow(false);
+                                findWinner();
+                            }}
+                        >
+                            Đóng
+                        </button>
+                    </div>
+                    <div className="mask"></div>
+                </>
+            ) : (
+                <div></div>
+            )}
+        </>
+    );
 }
 
 export default App;
